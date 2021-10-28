030d59d60763619b277bc5654b2912111e648f70
dm0n-hue <61423934+dm0n-hue@users.noreply.github.com>
2021-10-28 15:30:54 -0400

    Update guess_the_num_forloop.py

--- guess_the_num_forloop.py
+++ guess_the_num_forloop.py
@@ -4,6 +4,7 @@ print('Hello. What is your name?')
 name = input()
 
 print('Well, ' + name + ', I am thinking of a number between 1 and 20')
+print('You will have exactly 7 tries to figure out the number I am thinking of')
 secretNumber = random.randint(1,20)
 
 for guessesTaken in range(1,7):
